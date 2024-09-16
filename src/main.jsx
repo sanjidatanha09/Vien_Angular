a4c47e42dfb1225303b7a9e16aeb53872d03443e
Sanjida Akter <sanjidatanha09@gmail.com>
2024-09-16 17:49:54 +0600

    uhk

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { StrictMode } from "react";
+import Router from "./Router/Router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+            <RouterProvider router={Router} />
+            <ToastContainer />
+    </QueryClientProvider>
+  </StrictMode>
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,76 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#F3F4F6',
+        secondary: '#0771BA',
+        
+
+
+      },
+      margin: {
+        m_primary: '6px',
+        m_8px: '8px',
+        m_medium: '12px',
+        m_large: '20px',
+        m_secondary: '40px',
+        m_24px: '24px',
+        m_40px: '40px',
+
+      },
+      gap: {
+        gap_6px: '6px',
+        gap_8px: '8px',
+        gap_primary: '12px'
+      },
+      fontSize: {
+        text_small: '12px',
+        text_standard: '14px',
+        text_sm2: '15px',
+        text_medium: '16px',
+        text_title: '18px',
+        text_large: '20px',
+        text_xl: '24px',
+        text_30px: '30px',
+        text_xxl: '34px',
+        text_40px: '40px',
+        text_xxxl: '50px',
+
+      },
+      padding: {
+        p_3px: '3px',
+        p_4px: '4px',
+        p_primary: '6px',
+        p_8px: '8px',
+        p_10px: '10px',
+        p_12px: '12px',
+        p_16px: '16px',
+        p_primary2: '18px',
+        p_secondary: '20px',
+        p_24px: '24px',
+        p_28px: '28px',
+        p_30px: '30px',
+        p_40px: '40px',
+      },
+      borderRadius: {
+        rounded_4px: '4px',
+        rounded_primary: '6px',
+        rounded_secondary: '10px',
+        rounded_medium: '12px',
+      },
+      height: {
+        primary: '40px',
+        h_secondary: '192px'
+
+      },
+
+    },
   },
   plugins: [],
 }
-
